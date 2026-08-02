454931ec1dab53b1efdb93ead2566830d7be9cfb
AQ251351 <AQ251351@aquinas.ac.uk>
2026-08-02 15:08:23 +0100

    Add project files.

---
+++ CheckersNEA/Piece.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckersNEA
+{
+
+
+    public class Piece
+    {
+        public string Colour { get; set; }
+
+        public bool IsKing { get; set; }
+
+        public Piece(string colour)
+        {
+            Colour = colour;
+            IsKing = false;
+        }
+        public void MakeKing()
+        {
+            IsKing = true;
+        }
+    }
+}
---
+++ CheckersNEA/Program.cs
@@ -0,0 +1,2 @@
+﻿using var game = new CheckersNEA.Game1();
+game.Run();
